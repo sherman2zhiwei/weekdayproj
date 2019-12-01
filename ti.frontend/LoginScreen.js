7387fe81777e5178a6a3b81dadc7d27ff2004745
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2019-12-01 16:39:21 +0800

    Completed Google Login feature for standalone app

--- ti.frontend/LoginScreen.js
+++ ti.frontend/LoginScreen.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
 import * as Google from 'expo-google-app-auth';
 
+
+
+
 async function signInWithGoogleAsync(){
   try {
-    const result = await Google.logInAsync({
-      androidClientId: "1060561981131-10182gmn58ekeuorqu3pr6gqbkkj5bgo.apps.googleusercontent.com",
+    const config = {
+      expoClientId: "",
       iosClientId: "1060561981131-5fp82r6tul3l7nardmt7ed1t71i43f0q.apps.googleusercontent.com",
-      scopes: ['profile', 'email'],
-    });
+      androidClientId: "1060561981131-10182gmn58ekeuorqu3pr6gqbkkj5bgo.apps.googleusercontent.com",
+      iosStandaloneAppClientId: "",
+      androidStandaloneAppClientId: "1060561981131-a8im70tqcv8qpch6o6qdip7tgf77qj3u.apps.googleusercontent.com",
+    };
 
-    if (result.type === 'success') {
-      return result.accessToken;
-    } else {
-      return { cancelled: true };
+    const { type, accessToken } = await Google.logInAsync(config);
+
+    if (type === 'success') {
+      /* Log-Out */
+      await Google.logOutAsync({ accessToken, ...config });
+      /* `accessToken` is now invalid and cannot be used to get data from the Google API with HTTP requests */
     }
+
   } catch (e) {
     return { error: true };
   }

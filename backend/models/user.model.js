72d340dc211af7461df6770bf04f720e4450f667
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2019-12-05 21:36:44 +0800

    Implemented login API

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -48,5 +48,15 @@ function validateUser(user) {
   return Joi.validate(user, schema);
 }
 
+function validateLogin(user){
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(3).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
 exports.User = User; 
 exports.validate = validateUser;
+exports.validateLogin = validateLogin;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,14 +1,14 @@
 const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
-const { User, validate } = require("../models/user.model");
+const { User, validate, validateLogin } = require("../models/user.model");
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/users/:id", auth, async (req, res) => {
-  if (req.params.id != req.user._id) return res.status(401).send("Access denied. Not your user profile.");
-  const user = await User.findById(req.user._id).select("-password");
-  res.send(user);
-});
+// router.get("/:id", auth, async (req, res) => {
+//   if (req.params.id != req.user._id) return res.status(401).send("Access denied. Not your user profile.");
+//   const user = await User.findById(req.user._id).select("-password");
+//   res.send(user);
+// });
 
 router.post("/", async (req, res) => {
   // validate the request body first
@@ -24,9 +24,8 @@ router.post("/", async (req, res) => {
     password: req.body.password,
     email: req.body.email
   });
-  user.password = await bcrypt.hash(user.password, 10);
+  user.password = bcrypt.hashSync(user.password, 10);
   await user.save();
-
   const token = user.generateAuthToken();
   res.header("x-auth-token", token).send({
     _id: user._id,
@@ -35,4 +34,30 @@ router.post("/", async (req, res) => {
   });
 });
 
+
+router.post("/login", async (req, res) => {
+  // validate the request body first
+  const { error } = validateLogin(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  //find an existing user
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("User not found");
+
+  correct_password = user.password
+
+  console.log(correct_password)
+  console.log(user.password)
+
+
+  if (!bcrypt.compareSync(req.body.password, correct_password, 10)) return res.status(400).send("Wrong password");
+
+  const token = user.generateAuthToken();
+
+  res.header("x-auth-token", token).send({
+    _id: user._id,
+    name: user.name,
+    email: user.email
+  });
+
+})
 module.exports = router;
